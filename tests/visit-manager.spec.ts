ea9de0ee084069fdae6e4525a9253df8868bb40a
seesi <seesi@accede.dev>
2024-11-25 18:56:28 +0000

    Adds test to verify manager is able to edit booking

--- tests/visit-manager.spec.ts
+++ tests/visit-manager.spec.ts
@@ -89,19 +89,109 @@ test.describe("Verify that an Estate manager", () => {
 
   });
 
-  test.afterEach(async ({page})=>{
-    await page.getByRole('tab', { name: /Saved Visitors*/ }).click();
-    await page.locator('div > .dropdown > .i-false').first().click();
-    await page.getByRole('button', { name: 'View Details' }).click();
-    await page.getByText('Delete', { exact: true }).click();
-    await page.getByRole('button', { name: 'Delete' }).click();
-    await page.getByRole('button', { name: 'Done' }).click();
-  
+  test("Sends an email invite successfully to a new general visitor", async ({
+    page,
+  }) => {
+    await page.getByRole("button", { name: "New Visitor" }).click();
+    await page.getByLabel("Category").click();
+
+    await page.getByRole("option",{name: "General Visitor"}).click();
+    await page.getByRole('combobox', { name: 'Select Invite Source ​' }).click();
+    await page.getByRole("option",{name: "New"}).click();
+    await page.locator('div').filter({ hasText: /^visitor information \*$/ }).getByRole('img').click();
+    await page.getByLabel('First Name').click();
+    await page.getByLabel('First Name').fill('Nana');
+    
+    await page.getByLabel('Last Name').click();
+    await page.getByLabel('Last Name').fill('Addo');
+    
+    await page.locator('input[name="phoneNumber"]').click();
+    await page.locator('input[name="phoneNumber"]').fill('+233 20 123 4567');
+    
+    await page.getByLabel('Email(Optional)').click();
+    await page.getByLabel('Email(Optional)').fill('nanaaddo@yopmail.com');
+
+    await page.getByLabel('Gender').click();
+    await page.getByRole("option",{name: "Male", exact: true}).click();
+
+    await page.locator('div').filter({ hasText: /^Additional information\(optional\)$/ }).getByRole('img').click();
+    await page.getByPlaceholder('Enter a location').click();
+
+    await page.getByPlaceholder('Enter a location').fill('Flagstaff house ghana');
+    await page.getByText('Flagstaff HouseLiberation').click();
+
+    await page.locator('div').filter({ hasText: /^Visitor Booking Validity\*$/ }).getByRole('img').click();
+    await page.getByPlaceholder('Start Date').click();
+    await page.getByText('26').first().click();
+    await page.getByText('27').first().click();
+    await page.locator('div').filter({ hasText: /^Start Time$/ }).getByLabel('Choose time').click();
+    await page.getByLabel('3 hours', { exact: true }).click();
+    
+    await page.getByLabel('PM', { exact: true }).click();
+    await page.locator('div').filter({ hasText: /^End Time$/ }).getByLabel('Choose time, selected time is').click();
+    await page.getByLabel('6 hours',{ exact: true }).click();
+    await page.getByLabel('PM', { exact: true }).click();
+    
+    await page.getByLabel('No of Entries').click();
+    await page.getByRole('option',{name: 'Single entry'}).click();
+    
+    await page.locator('div').filter({ hasText: /^Send email with invite details to indicated email address$/ }).getByRole('checkbox').check();
+
+    await page.getByRole('button', { name: 'Book Visitor' }).click();
+  });
+
+  test("Can edit a booking.",async ({ page }) => {
     await page.getByRole('tab', { name: 'My Visitors' }).click();
     await page.getByRole('tab', { name: 'Upcoming' }).click();
-    await page.getByRole('row', { name: 'Nana Addo Liberation Rd,' }).getByLabel('Row Actions').click();
-    await page.getByText('Cancel').click();
-    await page.getByRole('button', { name: 'Yes' }).click();
+    
+    await page.getByLabel('Row Actions').click();
+    
+    await page.getByRole('menuitem', { name: 'Edit' }).click();
+
+    await page.locator('div').filter({ hasText: /^Visitor Booking Validity$/ }).getByRole('img').click();
+    await page.locator('div').filter({ hasText: /^Start Time$/ }).getByLabel('Choose time').click();
+    await page.getByLabel('6 hours', { exact: true }).click();
+    
+    await page.getByLabel('PM', { exact: true }).click();
+    await page.locator('div').filter({ hasText: /^End Time$/ }).getByLabel('Choose time, selected time is').click();
+    await page.getByLabel('9 hours',{ exact: true }).click();
+    await page.getByLabel('PM', { exact: true }).click();
+
+    await page.getByRole('button', { name: 'Update' }).click();
+    
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+      - paragraph: Successfully Updated
+      - paragraph: You have successfully updated this visitor’s booking.
+      - button "Okay"
+      `);
+
+    await page.getByRole('button', { name: 'Okay' }).click();
+    
+    await expect(page.locator('tbody')).toMatchAriaSnapshot(`
+      - cell /\\d+\\/\\d+\\/\\d+ 6:\\d+ PM/:
+        - paragraph: /\\d+\\/\\d+\\/\\d+ 6:\\d+ PM/
+      `);
+    await expect(page.locator('tbody')).toMatchAriaSnapshot(`
+      - cell /\\d+\\/\\d+\\/\\d+ 9:\\d+ PM/:
+        - paragraph: /\\d+\\/\\d+\\/\\d+ 9:\\d+ PM/
+      `);
+  })
+
+  test.afterEach(async ({page})=>{
+    /* Delete Saved Visitor */
+    // await page.getByRole('tab', { name: /Saved Visitors*/ }).click();
+    // await page.locator('div > .dropdown > .i-false').first().click();
+    // await page.getByRole('button', { name: 'View Details' }).click();
+    // await page.getByText('Delete', { exact: true }).click();
+    // await page.getByRole('button', { name: 'Delete' }).click();
+    // await page.getByRole('button', { name: 'Done' }).click();
+  
+    /* Cancel Booking */
+    // await page.getByRole('tab', { name: 'My Visitors' }).click();
+    // await page.getByRole('tab', { name: 'Upcoming' }).click();
+    // await page.getByRole('row', { name: 'Nana Addo Liberation Rd,' }).getByLabel('Row Actions').click();
+    // await page.getByText('Cancel').click();
+    // await page.getByRole('button', { name: 'Yes' }).click();
   })
 });
 
